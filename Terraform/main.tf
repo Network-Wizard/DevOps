a47ca14717620dfd35add2dede878b78daca55a0
m.imanpour <mahdi.emanpour@gmail.com>
2024-08-16 22:12:29 +0330

    Terraform basics with docker provider

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,23 +1,14 @@
-# Define a Docker image resource for Alpine Linux
 resource "docker_image" "alpine" {
   name = "alpine:latest"
 }
 
-# Create a Docker container from the Alpine image
 resource "docker_container" "alpine_container" {
   name  = "my_alpine_container"
-  image = docker_image.alpine.latest
-  
-  # Command to run when the container starts
+  image = docker_image.alpine.name  # Use name instead of id
   command = ["sh", "-c", "while true; do echo hello world; sleep 5; done"]
-
-  # Optional: You can specify a working directory, environment variables, etc.
   working_dir = "/tmp"
-
-  # Example environment variable
   env = ["EXAMPLE_ENV_VAR=TerraformDocker"]
 
-  # Optional: Limit container's resources (e.g., memory, CPU)
-  memory = "128m"
+  memory = 134217728  # 128 MB in bytes
   cpu_shares = 256
 }
